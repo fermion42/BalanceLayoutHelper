72b3aab6e940b785c5ed0da6d31197ca255a9b7c
Gulakya Young <glaxxxya@gmail.com>
2015-03-14 23:46:40 +0800

    Initial Commit

---
+++ BalanceViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BalanceViewDemo
+//
+//  Created by Yang Han on 15/3/14.
+//  Copyright (c) 2015年 Yang Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BalanceViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BalanceViewDemo
+//
+//  Created by Yang Han on 15/3/14.
+//  Copyright (c) 2015年 Yang Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
